05a5c5f01038ff3fa12cad7a307f6de33b5c7c5d
TimRCAnderson <timothyrcanderson@gmail.com>
2017-03-17 18:18:44 -0400

    Basic files setup.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,16 @@
+var player = {
+  health: 20,
+  strength: 10,
+  perception: 10,
+  endurance: 10,
+  charisma: 10,
+  intelligence: 10,
+  agility: 10,
+  luck: 10,
+  inventory: [],
+  money: 10
+};
+
+$(document).ready(function() {
+
+});
