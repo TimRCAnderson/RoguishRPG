0d310ab0f3bb576d92173f37c7e9d960cd4cea94
TimRCAnderson <timothyrcanderson@gmail.com>
2017-10-25 09:25:42 -0400

    Objects are being reworked to be ES5 classes for multiple instantiations, with private attributes and public getters and setters. Graphical display will be using WebGL through a canvas element.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,34 +1,46 @@
-var menu = $("#menu");
-var playArea = $("#play-area");
+function Inventory() {
+  this.items = [];
+};
 
-var player = {
-  health: 20,
-  armor: 10,
-  strength: 10,
-  perception: 10,
-  endurance: 10,
-  charisma: 10,
-  intelligence: 10,
-  agility: 10,
-  luck: 10,
-  inventory: [],
-  money: 10,
-  toHit: 0,
-  damage: 10,
-  resistances: {
+function Player() {
+  var health = 20;
+  var maxHealth = 20;
+  var armor = 10;
+  var strength = 10;
+  var perception = 10;
+  var endurance = 10;
+  var charisma = 10;
+  var intelligence = 10;
+  var agility = 10;
+  var luck = 10;
+  var inventory = new Inventory();
+  var money = 10;
+  var accuracy = 0;
+  var damage = 10;
+  var resistances = {
     fire: 0.0,
     acid: 0.0,
     shock: 0.0,
-    psy: 0.0
-  },
-  experience: 0
+    poison: 0.0,
+    psy: 0.0,
+    bleed: 0.0,
+    stun: 0.0
+  };
+  var experience = 0;
+
+  this.getHealth = function() {
+    return health;
+  }
+
+  this.setHealth = function(newHealth) {
+    health = newHealth;
+  }
 };
 
-var cell = {
-  occupant: null,
-  width: 30,
-  height: 30,
-  sprite: ""
+function cell(_terrain) {
+  var occupant = null;
+  var type = _terrain.type || "Bare dirt";
+  var movement = _terrain.movement || 1.0;
 };
 
 var grid = {
@@ -42,7 +54,7 @@ var grid = {
     this.width = width;
     for(var i = 0; i < height; i++)
     {
-
+      grid
     }
   }
 };
@@ -63,6 +75,20 @@ var monster = {
   inventory: []
 };
 
-$(document).ready(function() {
+window.onload = function() {
+  var menu = document.querySelector("#menu");
+  var playArea = document.querySelector("#play-area");
 
-});
+  canvas = document.createElement("canvas");
+  canvas.setAttribute("width", Math.round(window.innerWidth * .66));
+  canvas.setAttribute("height", Math.round(window.innerHeight * .66));
+
+  playArea.append(canvas);
+
+  const gl = canvas.getContext("webgl");
+  if(!gl) {
+    alert("Unable to initialize webgl.");
+  }
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
